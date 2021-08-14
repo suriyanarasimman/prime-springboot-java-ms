2f5957c22f0b255c04ba892c029095a2ee4bc17c
Dhruva Raju <49dhruva@gmail.com>
2021-08-14 11:28:11 +0530

    feat(initial): base maven project set up

---
+++ src/main/java/com/primebanc/primespringbootjavams/PrimeSpringbootJavaMsApplication.java
@@ -0,0 +1,13 @@
+package com.primebanc.primespringbootjavams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeSpringbootJavaMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimeSpringbootJavaMsApplication.class, args);
+    }
+
+}
